411375fc4d7d0611f03e0946b6a3353f83dbeb31
piccolojnr <rahimdaud246@gmail.com>
2024-05-22 23:48:48 +0000

    chore: Refactor LRUCache put() and get() methods

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,38 +5,53 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """"""
+    """
+    Least Recently Used (LRU) Cache implementation.
+    """
 
     def __init__(self):
         super().__init__()
-        self.stack = []
+        self.keys = []
 
     def put(self, key, item):
         """
-        Add an item in the cache
+        Add an item in the cache.
+
+        Args:
+            key: The key of the item.
+            item: The item to be added.
+
+        Returns:
+            None
         """
         if key is None or item is None:
             return
 
-        if key in self.stack:
-            self.stack.remove(key)
-            self.stack.append(key)
-            self.cache_data[key] = item
-            return
+        if key in self.keys:
+            self.keys.remove(key)
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discard = self.stack.pop()
+            discard = self.keys.pop(0)
             del self.cache_data[discard]
             print("DISCARD: {}".format(discard))
 
         self.cache_data[key] = item
-        self.stack.append(key)
+        self.keys.append(key)
 
     def get(self, key):
         """
-        Get an item by key
+        Get an item by key.
+
+        Args:
+            key: The key of the item.
+
+        Returns:
+            The item associated with the key, or None if the key is not found.
         """
         if key is None or key not in self.cache_data:
             return None
+
+        self.keys.remove(key)
+        self.keys.append(key)
 
         return self.cache_data[key]
