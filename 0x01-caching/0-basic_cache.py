ce95fb562fa90fe9eda15f1f490975a1bfcb3b37
piccolojnr <rahimdaud246@gmail.com>
2024-05-22 22:37:10 +0000

    chore: Add basic caching classes and README file

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+BasicCache class
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class that inherits from BaseCaching and is a caching system
+
+    """
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" FIFO caching
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache defines:
+    - caching system
+    """
+
+    def __init__(self):
+        """
+        Initiliaze
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.queue:
+            self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = self.queue.pop(0)
+            del self.cache_data[discard]
+            print("DISCARD: {}".format(discard))
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
